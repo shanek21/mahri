7133412a335303f22eab06da1d713fcb3f693d9f
shanek21 <shane.kelly@students.olin.edu>
2016-03-02 23:21:44 -0500

    First servo control test file

---
+++ servoTest.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(18, 100)
+pwm.start(5)
+
+def update(angle):
+    duty = float(angle) / 10.0 + 2.5
+    pwm.ChangeDutyCycle(duty)
+
+update(100)
