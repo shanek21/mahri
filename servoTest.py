910676b220d6306fd7c038126aeb9dfa045c117e
shanek21 <shane.kelly@students.olin.edu>
2016-03-19 02:58:20 -0400

    Working on easy servo control with servo class.

--- servoTest.py
+++ servoTest.py
@@ -1,13 +1,48 @@
 import RPi.GPIO as GPIO
 import time
+import atexit
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+class Servo:
+	"""
+	A servo class to hold attributes and
+	functions for controlling Mahri's servos.
+	"""
+	def __init__(self, pin, angle):
+		"""
+		Initialize a servo. Also initizlizes
+		a given PWM pin to control the servo.
+		"""
+		self.pin = pin
+		self.angle = angle
+		GPIO.setup(pin, GPIO.OUT)
+		self.pwm = GPIO.PWM(pin, 100)
+		self.pwm.start(5)
 
-def update(angle):
-    duty = float(angle) / 10.0 + 2.5
-    pwm.ChangeDutyCycle(duty)
+	def set(self, angle):
+		"""
+		Set the angle of a servo.
+		"""
+		self.pwm.ChangeDutyCycle(float(angle) / 10.0 + 2.5)
+		self.angle = angle
 
-update(100)
+def main():
+	"""
+	Create servo objects that can be
+	easily controlled.
+	"""
+	# Define pin number mode
+	GPIO.setmode(GPIO.BCM)
+
+	# Initialize left and right servos
+	leftServo = Servo(18, 0)
+	rightServo = Servo(20, 20)
+
+def cleanup():
+	"""
+	Close GPIO pins after code is run.
+	"""
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	main()
+	atexit.register(cleanup)
